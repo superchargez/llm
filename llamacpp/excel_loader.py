7cbc7155a1e41b3f2d679e846e914c229ead1d71
Jawad Mansoor <supercharge1991@gmail.com>
2024-11-05 21:58:32 +0500

    modified:   llamacpp/excel_loader.py

--- llamacpp/excel_loader.py
+++ llamacpp/excel_loader.py
@@ -1,10 +1,9 @@
 from langchain_community.document_loaders import UnstructuredExcelLoader
 import os; os.chdir(r"../excel_files")
-print(os.getcwd())
-print(os.listdir())
-loader = UnstructuredExcelLoader("tickes_sales.xlsx", mode="elements")
+
+loader = UnstructuredExcelLoader("ticket_sales_report2.xlsx", mode="elements")
 docs = loader.load()
 
 print(len(docs))
 
-docs
+docs[1]
