fa6ddca2ca32858f06510b96c68187ed042d8f0d
Jawad Mansoor <supercharge1991@gmail.com>
2024-11-05 15:47:45 +0500

    modified:   complex_excel_bot.py

--- complex_excel_bot.py
+++ complex_excel_bot.py
@@ -30,6 +30,21 @@ from transformers.utils import get_json_schema
 
 app = FastAPI()
 
+system_prompt = Template("""You are an expert in composing functions. You are given a question and a set of possible functions. 
+Based on the question, you will need to make one or more function/tool calls to achieve the purpose. 
+If none of the functions can be used, point it out and refuse to answer. 
+If the given question lacks the parameters required by the function, also point it out.
+
+You have access to the following tools:
+<tools>{{ tools }}</tools>
+
+The output MUST strictly adhere to the following format, and NO other text MUST be included.
+The example format is as follows. Please make sure the parameter type is correct. If no function call is needed, please make the tool calls an empty list '[]'.
+<tool_call>[
+{"name": "func_name1", "arguments": {"argument1": "value1", "argument2": "value2"}},
+... (more tool calls as required)
+]</tool_call>""")
+
 def prepare_messages(
     query: str,
     tools: Optional[dict[str, Any]] = None,
@@ -402,6 +417,10 @@ class SmartExcelAgent:
 # Initialize the system
 excel_manager = SmartExcelManager()
 agent = SmartExcelAgent(model, tokenizer, excel_manager)
+
+class QueryRequest(BaseModel):
+    query: str
+    history: Optional[List[Dict[str, str]]] = None
 
 @app.post("/query")
 async def query_model(request: QueryRequest):
